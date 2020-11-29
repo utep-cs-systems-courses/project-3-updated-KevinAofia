d8069012c69943862c008c3eb0d03b7f3bfd40ff
KevinAofia <aofia21@yahoo.com>
2020-11-28 19:44:27 -0700

    merged in buzzer and state machine for siren() and dim() functions .... dim still not working correctly

---
+++ project_main/SM.h
@@ -0,0 +1,9 @@
+#ifndef stateMachine_included
+#define stateMachine_included
+
+void siren();
+
+extern char siren_state; //for our siren() function
+extern int frequency; //for our siren() function
+
+#endif // included
